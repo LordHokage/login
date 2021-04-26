08304e0ccf4a9626819e463706276e8ab577c6f9
LordHokage <rajivdahal89@gmail.com>
2021-04-26 10:24:30 +0545

    created main.js file

---
+++ main.js
@@ -0,0 +1,11 @@
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const submitForm = document.querySelector(".submit-form");
+
+subtmitForm.addEventListener("click", connectToServer);
+
+function connectToServer (){
+    emailVal = email.value;
+    passVal = password.value;
+    
+}
